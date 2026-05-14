cf756148c8753fe77e1f2fa5faa2e184b500edcf
leonvic13 <leonvic13@gmail.com>
2026-05-14 13:56:55 -0600

    make the order on ly on active products

--- scm/views/purchase/po_views.py
+++ scm/views/purchase/po_views.py
@@ -47,7 +47,7 @@ def po_select_provider(request):
         # Get products that have a ProductProvider entry for this provider
         from im.models import ProductProvider
         product_ids = ProductProvider.objects.filter(provider=provider).values_list('product_id', flat=True)
-        products = Product.objects.filter(id__in=product_ids)
+        products = Product.objects.filter(id__in=product_ids, active=True)
         
         # Filter by stock needed (faltante1 != 0 and != 'no')
         items = []
@@ -84,7 +84,7 @@ def po_items_list(request, provider_id):
     # Get products that have a ProductProvider entry for this provider
     from im.models import ProductProvider
     product_ids = ProductProvider.objects.filter(provider=provider).values_list('product_id', flat=True)
-    products = Product.objects.filter(id__in=product_ids)
+    products = Product.objects.filter(id__in=product_ids, active=True)
     
     # Prepare items data
     items_data = []
