e1a0f6c27603e450297103cad89fe35a3704fbf8
leonvic13 <leonvic13@gmail.com>
2026-05-23 14:49:48 -0600

    change name instead of full name

--- im/admin.py
+++ im/admin.py
@@ -67,7 +67,7 @@ class ProductProviderInline(admin.TabularInline):
 
 class productAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     search_fields=['name','category__name','brand__name','id','barcode','clave']
-    list_display=('id','clave','full_name','display_group','get_stock_ready_to_sale','costo','priceLista','priceListaGranel','priceMayoreo','active','sat','Granel_Item')
+    list_display=('id','clave','name','brand','display_group','get_stock_ready_to_sale','costo','priceLista','priceMayoreo','active','sat','Granel_Item')
     list_filter=('active', ProviderFilter, 'brand', 'category', 'group')
     resocurce_class = productResource
     ordering=('id','last_updated')
