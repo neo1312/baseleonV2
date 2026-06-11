a1ef15ea664d4e309bce4c701a8e113192439732
leonvic13 <leonvic13@gmail.com>
2026-06-11 17:18:08 -0600

    whosale vendor list

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'crm.middleware.WholesaleBuyerRestrictionMiddleware',
+    'crm.middleware.BuyerRestrictionMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
--- crm/middleware.py
+++ crm/middleware.py
@@ -1,18 +1,20 @@
 from django.shortcuts import redirect
 
 
-class WholesaleBuyerRestrictionMiddleware:
+class BuyerRestrictionMiddleware:
     """
-    Restricts WholesaleBuyer users to only access the wholesale product lookup page.
+    Restricts WholesaleBuyer and Buyer users to only access the wholesale product lookup page.
     All other URLs redirect to /wholesale/.
     Must be placed after AuthenticationMiddleware in MIDDLEWARE.
     """
+    RESTRICTED_ROLES = ('WholesaleBuyer', 'Buyer')
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         if request.user.is_authenticated:
-            if request.user.groups.filter(name='WholesaleBuyer').exists():
+            if request.user.groups.filter(name__in=self.RESTRICTED_ROLES).exists():
                 path = request.path_info
 
                 allowed_prefixes = (
