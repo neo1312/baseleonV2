08f0cbcafeb6fcdecc16575a5414313202f7aa27
leonvic13 <leonvic13@gmail.com>
2026-06-17 14:13:38 -0600

    isntantn and edit field orders easy

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('',include('statModul.urls')),
     path('pos/', include('pos.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
